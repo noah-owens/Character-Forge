c9714ade1ae0de3cf88c7a2b6fe23b7f9d83a928
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2022-05-30 09:17:32 -0600

    :tada: Begin character class structure

---
+++ app/src/main/java/Character/Forge/Data/CharClass.java
@@ -0,0 +1,8 @@
+package Character.Forge.Data;
+
+/**
+ * CharClass objects represent classes and subclasses, with their associated proficiencies and features
+ */
+public class CharClass {
+    private CharClassName name;
+}
