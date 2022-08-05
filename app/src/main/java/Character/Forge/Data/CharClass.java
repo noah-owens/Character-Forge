2de4ff303c9aa1697622d96e28a2f425ff75cc72
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2022-08-05 11:37:10 -0600

    :fire: Delete CharClassName enum to reduce unneeded complexity

--- app/src/main/java/Character/Forge/Data/CharClass.java
+++ app/src/main/java/Character/Forge/Data/CharClass.java
@@ -4,5 +4,5 @@ package Character.Forge.Data;
  * CharClass objects represent classes and subclasses, with their associated proficiencies and features
  */
 public class CharClass {
-    private CharClassName name;
+    private String name;
 }
